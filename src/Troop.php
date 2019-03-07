32f5c4cd278d0fc608e76f6f4d9f0a4171767846
Fiko Borizqy <Fikokuper@gmail.com>
2019-03-07 10:30:35 +0700

    added fromDec and toDec method to convert, decimal to troop decimal or from troop decimal to decimal.

--- src/Troop.php
+++ src/Troop.php
@@ -2,8 +2,34 @@
 
 namespace Troop;
 
+use Troop\TroopNonStatic;
+
 class Troop {
-	public function decToTro() {
-		return "welcome\n";
+
+	public $number;
+
+	public function __construct() {
+		$this->number = json_decode(file_get_contents(__DIR__ . "/number.json"), true);
+	}
+
+	public static function fromDec(int $dec) {
+		$number = (new self)->number;
+		// return strval($dec);
+		do {
+			$mod_temp = (isset($div_temp)? $div_temp: $dec)%62;
+			$div_temp = floor((isset($div_temp)? $div_temp: $dec)/62);
+			$temp = $number[$mod_temp] . (isset($temp)? $temp: '');
+		} while($div_temp > 0);
+		return $temp;
+	}
+
+	public static function toDec(String $tro) {
+		$number = (new self)->number;
+		$number_flip = array_flip($number);
+		for($i=0; $i<strlen($tro); $i++) {
+			$j = (isset($j)? $j: strlen($tro)) - 1;
+			$dec = (isset($dec)? $dec: 0) + ($number_flip[$tro[$i]] * pow(62, strlen($tro)-1-$i));
+		}
+		return $dec;
 	}
 }
