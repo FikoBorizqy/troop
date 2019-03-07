4d30c00c6609d53b39c27498b52f1b1aa74c9c82
Fiko Borizqy <Fikokuper@gmail.com>
2019-03-08 05:49:08 +0700

    Bugs Fixed: 1. dependencies php 7.0 fixed become php 5.3.0 or higher Improvements: 1. charMapping method, mapping character from decimal, vise versa 2. adding comments on code

--- src/Troop.php
+++ src/Troop.php
@@ -4,12 +4,39 @@ namespace Troop;
 
 class Troop {
 
-	public $number;
+	/**
+	 * BASE CHARACTER
+	 * 
+	 * all characters saved here, from 0 - Z (capital)
+	 * file is in ../inc/number.json
+	 */
+	protected $number;
 
+
+
+	/**
+	 * CLASS CONSTRUCTOR
+	 * ====================================
+	 * 
+	 * prepare all prerequisites data that Troop needed
+	 * - getting ../int/number.json => base character of 61
+	 */
 	public function __construct() {
 		$this->number = json_decode(file_get_contents(__DIR__ . "/../inc/number.json"), true);
 	}
 
+
+
+	/**
+	 * CONVERT FROM DECIMAL
+	 * ====================================
+	 * 
+	 * converting data from decimal (integer) number to Troop
+	 * decimal.
+	 * 
+	 * @param 	Integer		$dec		(required) decimal or integer number
+	 * @return 	String					Troopdecimal result of converting from decimal
+	 */
 	public static function fromDec(int $dec) {
 		$number = (new self)->number;
 		// return strval($dec);
@@ -21,6 +48,18 @@ class Troop {
 		return $temp;
 	}
 
+
+
+	/**
+	 * CONVERT TO DECIMAL
+	 * ====================================
+	 * 
+	 * converting data from Troop decimal number to decimal
+	 * number (integer).
+	 * 
+	 * @param 	String		$tro		(required) Troop decimal character
+	 * @return 	Integer					Decimal result of Troop
+	 */
 	public static function toDec(String $tro) {
 		$number = (new self)->number;
 		$number_flip = array_flip($number);
@@ -29,5 +68,26 @@ class Troop {
 			$dec = (isset($dec)? $dec: 0) + ($number_flip[$tro[$i]] * pow(62, strlen($tro)-1-$i));
 		}
 		return $dec;
+	}
+
+
+
+	/**
+	 * TROOP CHARACTER MAPPING
+	 * ====================================
+	 * 
+	 * - get troop character of number from 0-61.
+	 * - get decimal character of troop from 0-Z
+	 * 
+	 * @param 	Integer|String		$num		(required) decimal from 0-9 or troop from 0-Z
+	 * @param 	Boolean				$getDec		(default: false) false means getting troop character from decimal, vice versa.
+	 * @return 	Integer|String					character result of decimal or troopdecimal
+	 */
+	public static function charMapping($num, $getDec = false) {
+		if($getDec !== true) {
+			return (new static)->number[$num];
+		} else {
+			return array_flip((new self)->number)[$num];
+		}
 	}
 }
